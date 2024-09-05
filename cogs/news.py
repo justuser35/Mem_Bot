780f4cb147f3ff2c6198046f00d1fe15912143d5
justuser35 <116276411+justuser35@users.noreply.github.com>
2024-09-05 16:10:37 +0300

    добавляю bs4 в news

--- cogs/news.py
+++ cogs/news.py
@@ -1,11 +1,21 @@
 from disnake.ext import commands
 import disnake
+from bs4 import BeautifulSoup as bs
+
+list = []
+
+# get text y статей
+soup = bs(html_doc, 'html.parser')
+articles = bs.find_all('a', class_="tm-title__link")
+
+for link in articles:
+    list += статья + link.get('href')
 
 
 class News(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.a = soup.find_all
     @commands.slash_command(
         name="news",
         description="Создать новую публикацию новости"
